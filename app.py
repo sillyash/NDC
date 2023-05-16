78df20a4f7c1c9fd8a5d99523101966943594d50
Ash <59657132+sillyash@users.noreply.github.com>
2023-05-16 09:33:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import pyxel
+from elements import Car, Player
 
+#joueur = Player()
+WIDTH = 128
+HEIGHT = 72
+
+
+class App:
+    def __init__(self):
+        pyxel.init(WIDTH, HEIGHT, title="Titre", fps=60, quit_key=pyxel.KEY_ESCAPE, """display_scale=14""")
+        #pyxel.load("res.pyxres")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+App()
