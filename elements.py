ef9948ecc1df073436aef2842c4a6b95b110a9f3
Ash <59657132+sillyash@users.noreply.github.com>
2023-05-16 09:55:41 +0200

    Update elements.py

--- elements.py
+++ elements.py
@@ -1,1 +1,46 @@
+import pyxel
 
+
+class Car:
+    def __init__(self, x, y, w, h, life, sprite):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.life = life
+        self.sprite = sprite
+        self.sheet = True
+
+    def move(self):
+        self.x += 1
+
+    def draw(self):
+        if self.life > 0:
+            pass
+        pass
+
+
+class Player:
+    def __init__(self):
+        self.tir = False
+        self.life = 3
+        self.score = 0
+
+    def draw_visor(self):
+        pyxel.circb(x, y, 4, )
+        if self.tir:
+            pyxel.circb(x, y, 4,)
+
+    def draw_life(self):
+        for i in range(self.life):
+            pyxel.rect(2 + i * 15, 10, 10, 10, 9)
+
+    def fire(self, target):
+        self.tir = pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT)
+        if self.tir and self.hit(target):
+            return True
+
+    def hit(self, target):
+        return pyxel.mouse_x > target.x and pyxel.mouse_y > target.y and \
+            pyxel.mouse_x < target.x + target.w and \
+            pyxel.mouse_y < target.y + target.h
